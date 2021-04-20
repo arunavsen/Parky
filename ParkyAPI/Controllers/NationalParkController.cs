db65c888bbebfc18220bec40464f6f6482a3d7d0
Arunav <arunav@vonome.com>
2021-04-20 16:43:11 +0600

    Create National Park Controller

---
+++ ParkyAPI/Controllers/NationalParkController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParkController : ControllerBase
+    {
+        public INationalParkRepository _npRepo;
+        private IMapper _mapper;
+
+        public NationalParkController(INationalParkRepository npRepo, IMapper mapper)
+        {
+            _npRepo = npRepo;
+            _mapper = mapper;
+        }
+    }
+}
