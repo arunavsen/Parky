e36b185e5ddb9dfac1a4a3e4d9c7e9481aca616d
Arunav <arunav@vonome.com>
2021-04-23 20:19:49 +0600

    Trail Model and TrailDto V.2.0

--- ParkyAPI/Model/DTOs/TrailDto.cs
+++ ParkyAPI/Model/DTOs/TrailDto.cs
@@ -11,9 +11,12 @@ namespace ParkyAPI.Model.DTOs
     public class TrailDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public double Distance { get; set; }
         public DifficultyType Difficulty { get; set; }
+        [Required]
         public int NationalParkId { get; set; }
         public NationalParkDto NationalPark { get; set; }
     }
--- ParkyAPI/Model/Trail.cs
+++ ParkyAPI/Model/Trail.cs
@@ -9,7 +9,7 @@ namespace ParkyAPI.Model
 {
     public class Trail
     {
-        [Required]
+        [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
