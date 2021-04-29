148f46cc9a14f11c58cb52b65c3c598c5bd42222
Arunav Sen <arunav.sen@dimikit.com>
2021-04-29 11:13:44 +0600

    Add Images to NationalPark

--- ParkyAPI/Model/DTOs/NationalParkDto.cs
+++ ParkyAPI/Model/DTOs/NationalParkDto.cs
@@ -10,6 +10,7 @@ namespace ParkyAPI.Model.DTOs
         public int Id { get; set; }
         public string Name { get; set; }
         public string State { get; set; }
+        public byte[] Pictures { get; set; }
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
     }
--- ParkyAPI/Model/NationalPark.cs
+++ ParkyAPI/Model/NationalPark.cs
@@ -15,6 +15,8 @@ namespace ParkyAPI.Model
         public string Name { get; set; }
         [Required]
         public string State { get; set; }
+
+        public byte[] Pictures { get; set; }
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
     }
