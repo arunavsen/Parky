aecf7d3fb7655209af6a15d8236ca6f11069d14b
Arunav Sen <arunav.sen@dimikit.com>
2021-05-01 01:54:41 +0600

    NationalPark and Trail Repository

---
+++ ParkyWeb/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkyWeb.Models;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    interface INationalParkRepository : IRepository<NationalPark>
+    {
+        
+    }
+}
---
+++ ParkyWeb/Repository/IRepository/ITrailRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkyWeb.Models;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    interface ITrailRepository : IRepository<Trail>
+    {
+    }
+}
