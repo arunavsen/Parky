f4dafd951c0078c11b7498127a60ab5b62610761
Arunav Sen <arunav.sen@dimikit.com>
2021-05-01 00:54:50 +0600

    CreateAsync in Repository

---
+++ ParkyWeb/Repository/Repository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ParkyWeb.Repository.IRepository;
+
+namespace ParkyWeb.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public Repository(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<bool> CreateAsync(string url, T objectCreate)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if (objectCreate != null)
+            {
+                request.Content = new StringContent(JsonConvert.SerializeObject(objectCreate), Encoding.UTF8,"application/json");
+            }
+            else
+            {
+                return false;
+            }
+
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.Created)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Task<bool> DeleteAsync(string url, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(string url, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(string url, T objectCreate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
