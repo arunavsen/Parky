c45a58836242c20f4129b772f138634cca0dc0f5
Arunav Sen <arunav.sen@dimikit.com>
2021-05-05 20:54:57 +0600

    Upsert View Part 1

--- ParkyWeb/Repository/Repository.cs
+++ ParkyWeb/Repository/Repository.cs
@@ -78,7 +78,7 @@ namespace ParkyWeb.Repository
 
         public async Task<T> GetAsync(string url, int id)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var request = new HttpRequestMessage(HttpMethod.Get, url+id);
             var client = _clientFactory.CreateClient();
             HttpResponseMessage response = await client.SendAsync(request);
             if (response.StatusCode == HttpStatusCode.OK)
--- ParkyWeb/SD.cs
+++ ParkyWeb/SD.cs
@@ -8,8 +8,8 @@ namespace ParkyWeb
     public static class SD
     {
         public static string APIBaseUrl = "https://localhost:44378/";
-        public static string NationalParkAPIPath = APIBaseUrl+ "api/v1/nationalpark";
-        public static string TrailAPIPath = APIBaseUrl+ "api/v1/trails";
+        public static string NationalParkAPIPath = APIBaseUrl+ "api/v1/nationalpark/";
+        public static string TrailAPIPath = APIBaseUrl+ "api/v1/trails/";
 
     }
 }
