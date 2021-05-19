b10c7b50b50c087c88d252dc0dac3a34954390ce
Arunav Sen <arunav.sen@dimikit.com>
2021-05-19 17:31:48 +0600

    Add User and User Repository

---
+++ ParkyAPI/Repository/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkyAPI.Model;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public User Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
