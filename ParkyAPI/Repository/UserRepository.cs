eefdd7801026ed8ef409661d17a544c3df9b853f
Arunav Sen <arunav.sen@dimikit.com>
2021-05-25 14:34:01 +0600

    Create token on Successful Authentication

--- ParkyAPI/Repository/UserRepository.cs
+++ ParkyAPI/Repository/UserRepository.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 using ParkyAPI.Data;
 using ParkyAPI.Model;
 using ParkyAPI.Repository.IRepository;
@@ -11,15 +15,42 @@ namespace ParkyAPI.Repository
     public class UserRepository : IUserRepository
     {
         private readonly ApplicationDbContext _db;
+        private readonly AppSettings _appSettings;
 
-        public UserRepository(ApplicationDbContext db)
+
+        public UserRepository(ApplicationDbContext db, AppSettings appSettings)
         {
             _db = db;
+            _appSettings = appSettings;
         }
 
         public User Authenticate(string username, string password)
         {
-            throw new NotImplementedException();
+            var user = _db.Users.SingleOrDefault(x => x.UserName == username && x.Password == password);
+
+            //user not found
+            if (user == null)
+            {
+                return null;
+            }
+
+            //if user was found generate JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials
+                    (new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            return user;
         }
 
         public bool IsUniqueUser(string username)
